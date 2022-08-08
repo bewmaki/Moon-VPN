2ecd126cafa79f5b8be8a8d04ec3d1f6a23b15b0
bewmaki <106496857+bewmaki@users.noreply.github.com>
2022-08-08 22:58:29 +0700

    Update moon.php

--- moon.php
+++ moon.php
@@ -1,26 +1,31 @@
-{"Version_update": "1.1",
+{"Version_update": "1.2",
   "Link_zip": "https://github.com/bewmaki/Moon-VPN/raw/main/moon.zip",
   "Password": "bewmaki11",
   "Message": "⚠️ควรอ่านรายละเอียดก่อนใช้งาน
-🛑แจ้งรายการอัพเดท🔻
-°ヾสวัสดีสมาชิกMoon VPN💨
- 
-❥︎ ทำไมถึงต้องมีให้เพิ่มเวลาในแอพ 
 
+🛑แจ้งรายการอัพเดท🛑
+
+°ヾสวัสดีสมาชิกMoon VPN💨
+❥︎ ทำไมถึงต้องมีให้เพิ่มเวลาในแอพ 
 𓏸 เพื่อเป็นการสนับสนุนแอพของเรา
 𓏸 เซิร์ฟเวอร์ฟรีทั้งหมดในแอพมีค่าใช้จ่าย
 𓏸 การดูโฆษณาทำให้แอดมินมีรายได้เพื่อนำไปเปิดเซิร์ฟเวอร์มาให้ใช้เร็ว-แรง รองรับคนได้มากขึ้น เสถียรมากขึ้น
-
+𓏸 ดูโฆษณาครั้งละ 12 ชั่วโมง เก็บได้สูงสุด 72 ชั่วโมง⏰
 ☀︎︎ รบกวนกดดูโฆษณาวันละ 1-2 ครั้ง เพื่อเป็นการสนับสนุนแอพของเรา ให้พัฒนาไปได้ดียิ่งขึ้นด้วยนะครับ
 
 🌟 สมัคร VIP ติดต่อแอดมิน
 🔥 VIP เดือนละ 25 บาท💵
  
-📡 เซิร์ฟเวอร์ 24 เซิร์ฟเวอร์ 📡
-🇸🇬 SG TRUE OVH  - 4 server
-🇸🇬 SG DTAC OVH  - 4 server
-🇹🇭 TH  TRUE -  3 server 🎮
-🇹🇭 TH  DTAC -  3 server 🎮
+📡 เซิร์ฟเวอร์ 30 เซิร์ฟเวอร์ 📡
+🇸🇬 SG  AIS  OVH  - 2 server
+🇸🇬 SG  TRUE OVH  - 4 server
+🇸🇬 SG  DTAC OVH  - 4 server
+🇹🇭 TH  AIS  -  2 server
+🇹🇭 TH  TRUE -  3 server
+🇹🇭 TH  DTAC -  3 server
+🇬🇧 UK  AIS  -  2 server
+🇬🇧 UK  TRUE -  3 seever
+🇬🇧 UK  DTAC -  3 server
 🇹🇭 VIP AIS  -  1 server 
 🇹🇭 VIP TRUE -  1 server 
 🇹🇭 VIP DTAC -  1 server 
