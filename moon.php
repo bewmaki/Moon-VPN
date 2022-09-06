6a1dca11c1519633589c0c9caf665091bb50dcbc
bewmaki <106496857+bewmaki@users.noreply.github.com>
2022-09-06 21:25:24 +0700

    Update moon.php

--- moon.php
+++ moon.php
@@ -1,4 +1,4 @@
-{"Version_update": "3.1",
+{"Version_update": "3.2",
   "Link_zip": "https://github.com/bewmaki/Moon-VPN/raw/main/moon.zip",
   "Password": "bewmaki11",
   "Message": "⚠️ควรอ่านรายละเอียดก่อนใช้งาน
