e4ba6c497608c44d6bf4d45afbacf22f9db75087
bewmaki <106496857+bewmaki@users.noreply.github.com>
2023-03-22 23:07:21 +0700

    Update Shark.php

--- Shark.php
+++ Shark.php
@@ -1,4 +1,4 @@
-{"Version_update": "2.4",
+{"Version_update": "2.5",
   "Link_zip": "https://github.com/bewmaki/Moon-VPN/raw/main/Shark.zip",
   "Password": "bewmaki11",
   "Message": "🟣ควรอ่านรายละเอียดก่อนใช้งาน🟣
